01bfac80b486a8eab3b562cf99e73b2e225fc82e
chrishsieh <902006+chrishsieh@users.noreply.github.com>
2019-03-04 16:24:49 +0800

    Fix apache setting.

--- setting/php-apache-dev/app/updatessl.sh
+++ setting/php-apache-dev/app/updatessl.sh
@@ -6,6 +6,10 @@ DEFAULT_CONF="/etc/apache2/sites-available/000-default.conf"
 CERTS="/etc/apache2/certs"
 
 updatessl() {
+ssl=0
+cp /etc/apache2/sites-available/www.conf /etc/apache2/sites-available/ChurchCRM.conf
+sed -i "s/#ServerName www.example.com/ServerName ${VIRTUAL_HOST}/g" /etc/apache2/sites-available/ChurchCRM.conf
+sed -i "s\DocumentRoot /var/www/html\DocumentRoot ${VIRTUAL_ROOT}\g" /etc/apache2/sites-available/ChurchCRM.conf
 #service apache2 force-reload
 
 for site_info_base64 in $(cat /var/run/secrets/ssl | jq -r '.[] | @base64'); do
@@ -21,6 +25,7 @@ for site_info_base64 in $(cat /var/run/secrets/ssl | jq -r '.[] | @base64'); do
                 ssl_parameter_base64=$(_jq ${site_info_base64} '.'$(echo "${ssl_mode}"'|@base64'))
                 case $(_jq ${ssl_parameter_base64} '.mode') in
                     'dns')
+                        ssl=1
                         eval export $(jq -r '.Key1_name' /var/run/secrets/dns_api)=$(jq -r '.Key1_value' /var/run/secrets/dns_api)
                         eval export $(jq -r '.Key2_name' /var/run/secrets/dns_api)=$(jq -r '.Key2_value' /var/run/secrets/dns_api)
                         eval $ACME_BIN --issue --dns $(jq -r '.Provider' /var/run/secrets/dns_api) $(_jq ${ssl_parameter_base64} '.domain_parameter') $(_jq ${ssl_parameter_base64} '.extra_parameter') \
@@ -54,12 +59,14 @@ for site_info_base64 in $(cat /var/run/secrets/ssl | jq -r '.[] | @base64'); do
                 openssl x509 -req -passin pass:xxxx -in server.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out "$CERTS/$(_jq ${site_info_base64} '.host').crt.pem" -days 3600 -sha256 -extfile /app/v3.ext;
                 rm rootCA.*
                 rm server.csr
+                ssl=1
             else
                echo "skip updatessl"
             fi
             ;;
         'own')
             echo "own ssl mode"
+            ssl=1
             ;;
         'none')
             echo "no ssl"
@@ -70,6 +77,16 @@ for site_info_base64 in $(cat /var/run/secrets/ssl | jq -r '.[] | @base64'); do
     esac
 done
 
+a2ensite ChurchCRM.conf
+if [ ssl == 1 ]; then
+    cp /etc/apache2/sites-available/ssl.conf /etc/apache2/sites-available/ChurchCRM-ssl.conf
+    sed -i "s\DocumentRoot /var/www/html\DocumentRoot ${VIRTUAL_ROOT}\g" /etc/apache2/sites-available/ChurchCRM-ssl.conf
+    sed -i "s\SSLCertificateFile	/etc/ssl/certs/ssl-cert-snakeoil.pem\SSLCertificateFile	/etc/apache2/certs/${VIRTUAL_HOST}.crt.pem\g" /etc/apache2/sites-available/ChurchCRM-ssl.conf
+    sed -i "s\SSLCertificateKeyFile /etc/ssl/private/ssl-cert-snakeoil.key\SSLCertificateKeyFile /etc/apache2/certs/${VIRTUAL_HOST}.key.pem\g" /etc/apache2/sites-available/ChurchCRM-ssl.conf
+    a2enmod ssl
+    a2ensite ChurchCRM-ssl.conf
+fi
+a2enmod rewrite
 #service apache2 force-reload
 }
 
